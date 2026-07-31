604d2c809a6ff1ef0388c0c9b03309d5e7b58458
yuntaod <33884966+yuntaod@users.noreply.github.com>
2026-07-31 04:38:56 +0000

    Deploying to barron-pages from @ yuntaod/researchpage@b448f11d7d21d5754f4c6195b0e12f51daaac386 🚀

--- notes/ml/gitbook/gitbook-plugin-ga/plugin.js
+++ notes/ml/gitbook/gitbook-plugin-ga/plugin.js
@@ -1,1 +1,17 @@
-require(["gitbook"],function(e){e.events.bind("start",function(e,n){var a,t,o,i,c,s,g;a=window,t=document,o="script",i="//www.google-analytics.com/analytics.js",c="ga",a.GoogleAnalyticsObject=c,a[c]=a[c]||function(){(a[c].q=a[c].q||[]).push(arguments)},a[c].l=1*new Date,s=t.createElement(o),g=t.getElementsByTagName(o)[0],s.async=1,s.src=i,g.parentNode.insertBefore(s,g);var r=n.ga;ga("create",r.token,r.configuration)}),e.events.bind("page.change",function(){ga("send","pageview",window.location.pathname+window.location.search)})});
+require(["gitbook"], function(gitbook) {
+    // Load analytics.js
+    gitbook.events.bind("start", function(e, config) {
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        var cfg = config.ga;
+        ga('create', cfg.token, cfg.configuration);
+    });
+
+    // Notify pageview
+    gitbook.events.bind("page.change", function() {
+        ga('send', 'pageview', window.location.pathname+window.location.search);
+    });
+});
